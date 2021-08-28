4682f9f92b203a5dd6a5909699a4536fa0cfb713
joyryder <shrifashis@gmail.com>
2021-08-27 23:40:11 -0400

    broken code for group creation

---
+++ src/components/ItemBox.js
@@ -0,0 +1,59 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const ItemBox = (props) => {
+  const leftSwipe = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [1, 0],
+      extrapolate: 'clamp',
+    });
+    return (
+      <TouchableOpacity onPress={console.log('press')} activeOpacity={0.6}>
+        <View style={styles.deleteBox}>
+          <Animated.Text style={{transform: [{scale: scale}]}}>
+            Delete
+          </Animated.Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <Swipeable renderRightActions={leftSwipe}>
+      <View style={styles.container}>
+        <Text>{props.data.name}.</Text>
+      </View>
+    </Swipeable>
+  );
+};
+
+export default ItemBox;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    width: SCREEN_WIDTH,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  deleteBox: {
+    borderWidth:5,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 80,
+  },
+});
